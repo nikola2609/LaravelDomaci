8d3f51fe7466ea0cf2db5aaa440f5e74e5901820
Nikola Lazovic <nikolalazovic26@gmail.com>
2022-05-12 11:05:21 +0200

    Izmena databaseSeedera

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\StudentSeeder;
+use Database\Seeders\IspitSeeder;
+use Database\Seeders\PrijavaSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $StudentSeeder=new StudentSeeder;
+        $IspitSeeder=new IspitSeeder;
+        $PrijavaSeeder=new PrijavaSeeder;
+
+        $StudentSeeder->run();
+        $IspitSeeder->run();
+        $PrijavaSeeder->run();
+
     }
 }
