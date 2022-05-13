5a6d3fac47c67ac88e7b3e1b3ee3037ac4548a3c
Nikola Lazovic <nikolalazovic26@gmail.com>
2022-05-13 13:15:14 +0200

    Kreiran AuthContoller

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('MojToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response()->json($response);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response(['message' => 'Wrong credentials!']);
+        } else {
+            $token = $user->createToken('MojLoginToken')->plainTextToken;
+            $response = [
+                'user' => $user,
+                'token' => $token
+            ];
+
+            return response()->json($response);
+        }
+    }
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json(['Logout successfully!']);
+    }
+
+
+
+}
